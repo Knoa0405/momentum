ba0d6159c31d0cfa77b9ce5be383b4b2a6ca247d
wowgod9504 <mike1124@naver.com>
2020-06-14 01:37:20 +0900

    create basic html,css,js and weather API

---
+++ src/weather.js
@@ -0,0 +1,53 @@
+const COORDINATES = "coordinates";
+const API_KEY = "5d89c497bb40c3ac4100a842fdd5c3ec";
+const weather = document.querySelector(".js-weather");
+
+function getWeather(lat,lng){
+    fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json()
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature}℃ ${place}`;
+    });
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude : latitude,
+        longitude : longitude
+    }
+    getWeather(latitude,longitude);
+    localStorage.setItem(COORDINATES,JSON.stringify(coordsObj));
+}
+
+function handleGeoError() {
+    weather.innerText = "Location and Weather not found error";
+}
+
+function askForCoordinate() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError)
+}
+
+function loadCoordinate() {
+    const loadedCoordinates = localStorage.getItem(COORDINATES);
+    if(loadedCoordinates === null) {
+        askForCoordinate();
+    } else {
+    const parseCoords = JSON.parse(loadedCoordinates)
+    getWeather(parseCoords.latitude,parseCoords.longitude);
+    }
+}
+
+
+
+
+
+function init() {
+    loadCoordinate();
+}
+init();
